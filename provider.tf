8f04243e486cae436e1b041a51607f61d4d0fc69
Abhishek Juyal <juyal.abhishek75@gmail.com>
2024-12-30 10:59:43 +0530

    v1_inspector

--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
-/*provider "aws" {
+provider "aws" {
     region = var.aws_region
   assume_role {
-    role_arn = "arn:aws:iam::384961893048:role/TerraformRole_admin"
+    role_arn = var.Terraform_role
   }
-}*/
+}
 
-provider "aws" {
+/*provider "aws" {
   region = "us-east-1"
   access_key = ""
   secret_key = ""
-}
+}*/
